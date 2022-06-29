ac54a75a3c14f1de825ef4d0176b7997e9ed25fa
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-06-29 15:20:01 -0700

    edit task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	char message[] = "\"Programming is like building a multilingual puzzle";
-
-        puts(message);
-        return (0);
+	
+       puts(message);
+       return (0);
 }
