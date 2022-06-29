6de690407a75b28b1b21d960836da2ab8cfd63d2
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-06-29 12:39:21 -0700

    firs set of scripts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - this is a program that prints a message
+ * Return: returns 0 if message prints successfully
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
