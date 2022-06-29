70f7611091b8c3566e54abc0b3ef2cb2037543b6
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-06-29 16:22:53 -0700

    final 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %zu byte (s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
