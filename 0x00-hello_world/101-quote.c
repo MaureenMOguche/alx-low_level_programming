0d0126655543f9ce45229288129652a7f585d9fb
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-06-29 18:15:08 -0700

    hmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
  * Return: returns a value of 1 when it runs successfully
  */
 #include <stdio.h>
+#include <unistd.h>
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
