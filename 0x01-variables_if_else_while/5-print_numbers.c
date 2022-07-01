ad2457e82f8a87ca7a492ba1f4b2ffbc47eae78d
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-01 07:57:09 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a ++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
