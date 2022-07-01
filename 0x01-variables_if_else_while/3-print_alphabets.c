d5b0a239d881210a27c5eedcc6ef52b2b4b31b96
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-01 05:56:25 -0700

    task 3 final

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,8 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-		for (ca = 'A'; ca <= 'Z'; ca++)
-			putchar(ca);
+	for (ca = 'A'; ca <= 'Z'; ca++)
+		putchar(ca);
 	putchar('\n');
 	return (0);
 }
