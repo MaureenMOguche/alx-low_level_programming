1277a5cbc09ef8f3da2c414168004ec331bf8b05
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-01 09:44:33 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print allpossible single digit numbers
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		putchar(a);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
