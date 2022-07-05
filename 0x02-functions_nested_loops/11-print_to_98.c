b841b632e9b23f81da21257d86fa56c965f30136
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 12:16:01 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.c"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: value to start printing from
+ */
+void print_to_98(int n);
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar((n/10) + '0');
+			_putchar((n%10) + '0');
+			n++;
+		}
+	}
+	else if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			_putchar((n/10) + '0');
+			_putchar((n%10) + '0');
+			n--;
+		}
+	}
+}
+
