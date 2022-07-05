f236b9153d294de3b5d7f66edad4bc8a1bd9b923
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 09:46:02 -0700

    okay

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,18 @@
 /**
  * print_last_digit - prints the last digit of a number
  *
+ * @n: number whose last digit is to be printed
+ *
  * Return: returns value of the last digit
  */
 int print_last_digit(int n)
 {
-	int nac;
+	int nac, lastdigit;
 
-	n = n % 10;
-	nac = n + 48;
+	lastdigit = n % 10;
+	nac = lastdigit + 48;
 	_putchar(nac);
-	return (n);
+	return (lastdigit);
 }
 
 
