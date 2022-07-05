851bdae541cc69341f5984433686743e7b88a3f2
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 12:34:49 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,17 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			if (n == 98)
-				break;
-			else
-				continue;
-			_putchar(' ');
-			_putchar('\n');
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar('\n');
+			printf("%d, ", n);
 			n--;
 		}
 	}
