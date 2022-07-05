0fced21d8d5ca010fe1722790ad1b25eb1bbfd3d
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 08:13:18 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- * _islower(int c) - checks for lower case characters
+ * _islower - checks for lower case characters
+ *
+ * Return: returns 1 if c is lower.
+ *
+ * c - value to be tested.
  */
 int _islower(int c)
 {
-	if(c > 97 && c < 123)
+	if (c > 97 && c < 123)
 		return (1);
 	else
 		return (0);
