4bb64babd384c177818b08b8ad48c5c05ee8c8ed
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 08:45:28 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: the value to be tested
+ *
+ * Return: returns zero if n is 0, returns -1 if n is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return(0);
+	}
+	else
+	{
+		_putchar(45);
+		return(-1);
+	}
+}
