641fde96c3082796b727c243962b02082fedcb1d
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-05 19:53:15 -0700

    jack b

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			_putchar((hr % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
-			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 			min++;
 		}
