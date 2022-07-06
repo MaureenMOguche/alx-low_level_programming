d8e22645dcd7fcb38b44bf2ff005790fdfaeb1b4
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-06 02:38:02 -0700

    two

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,11 +10,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && b < > c)
 		largest = a;
-	else if (b > a && a > c)
+	else if (b > a && a < > c)
 		largest = b;
-	else if (c > a && a > b)
+	else if (c > a && a < > b)
 		largest = c;
 	return (largest);
 }
