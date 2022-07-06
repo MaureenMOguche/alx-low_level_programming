d2dfa64cea2ccb7e008ddc262806e38ab51191f4
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-06 02:46:34 -0700

    new

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,10 +11,16 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && (b < c || b > c))
+	{
 		largest = a;
+	}
 	else if (b > a && (a < c || a > c))
+	{
 		largest = b;
+	}
 	else if (c > a && (a < b || b > a))
+	{
 		largest = c;
+	}
 	return (largest);
 }
