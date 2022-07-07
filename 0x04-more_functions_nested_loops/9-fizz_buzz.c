cd545ce7e52dec9ff54f61ba2e749826c46bae8a
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-07 15:32:40 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,16 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0)
-			printf("%s ", "Fizz");
+			printf("%s", "Fizz");
 		else if (i % 5 == 0)
-			printf("%s ", "Buzz");
+			printf("%s", "Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("%s ", "FizzBuzz");
+			printf("%s", "FizzBuzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i < 99)
+			printf("%s", " ");
 	}
 	printf("\n");
+	return (0);
 }
