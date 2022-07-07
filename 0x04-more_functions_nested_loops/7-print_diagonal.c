d4504829b3fac4fc772dffa0fa8b5225bf4087e7
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-07 15:24:25 -0700

    seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,10 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = i; j >= 0; j--)
+			for (j = i-1; j >= 0; j--)
 			{
 				_putchar(32);
+			/*	_putchar(92);*/
 			}
 			_putchar(92);
 			_putchar('\n');
