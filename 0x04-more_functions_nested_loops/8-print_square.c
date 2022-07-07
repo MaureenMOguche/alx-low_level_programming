4f01e511ce292053517beace9ccebb05c4dac720
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-07 13:58:43 -0700

    eight

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		if (size <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
