13db2a49f0b36071a3ff9cb8e5cd25095694ba14
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-07 09:27:00 -0700

    three

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	a * b;
+	return (0);
+	break;
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 -9
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; c++)
+		_putchar(c);
+	_putchar('\n');
+}
