b864767a223343a41a4f79b346fb6b5321b5d97b
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-07 20:34:27 -0700

    extra

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
