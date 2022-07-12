b68c8a82311a9bfe1c9178200c65daae6cefc1f8
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-12 15:38:54 -0700

    seven

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rev_string - reverses a string
@@ -13,7 +12,5 @@ void rev_string(char *s)
 	
 	for (i = 0; s[i] != '\n'; i++)
 		;
-	for (j = i; j >= 0; j--)
-		printf("%s", s[j]);
-	printf("\n");
+	for (j = i; j >= 0; j--):
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,18 @@
 void puts_half(char *str)
 {
 	int len, n;
-	for (len = 0; str[len] != 0; i++)
+
+	for (len = 0; str[len] != 0; len++)
 		;
-	for (n = 0; n <= len; n++)
+	if (len % 2 == 0)
 	{
-		if (len % 2 == 0)
-			n = len/2;
-		else
-			n = (len - 1)/2;
-		_putchar(str[n]);
+		for (n = (len / 2); n < len; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = (len - 1) / 2; n < len; n++)
+			_putchar(str[n]);
 	}
 	_putchar('\n');
 }
