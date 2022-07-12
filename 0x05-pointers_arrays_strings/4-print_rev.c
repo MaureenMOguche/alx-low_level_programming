6945c717327fb1316325a14d4c79be5ccf7751fc
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-12 15:48:19 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 		;
-	for (j = i; j > 0; j--)
+	for (j = i; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
