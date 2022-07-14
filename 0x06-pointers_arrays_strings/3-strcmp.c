60d1e877b0a78c1836f8b64b6db85b2bb9aafbe1
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-14 13:06:16 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,11 +19,11 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (s1[i] < s2[i])
 		{
-			j = 15;
-		}
-		else
-		{
 			j = -15;
+		}
+		else if (s1[i] > s2[i])
+		{
+			j = 15;
 		}
 	}
 	return (j);
