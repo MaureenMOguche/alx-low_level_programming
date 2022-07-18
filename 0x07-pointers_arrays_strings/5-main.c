da5f02554da01df546d1a41311d787038e1f47e1
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-18 13:22:30 -0700

    five

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -15,4 +15,4 @@ int main(void)
     t = _strstr(s, f);
     printf("%s\n", t);
     return (0);
-}}
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ *
+ * @a[8]: an element in the chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	char c;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			c = a[i][j];
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
