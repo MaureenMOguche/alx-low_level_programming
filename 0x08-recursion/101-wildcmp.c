68140570049c06dc8498b6ca8b2763c3f86dfdbe
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-20 20:17:55 -0700

    8; ;

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: 1 if identical else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
