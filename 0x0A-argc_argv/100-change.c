8fb3f012216290464f8c73b5a574b7f7e41e8b0a
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-22 15:56:11 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -8,20 +9,45 @@
 
 int main(int argc, char **argv)
 {
-	
-	if (argc != 2)
+	int coins = 0;
+	int cents;
+
+	if (argc == 2)
+	{
+		cents = atoi(argv[1]);
+		if (cents < 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		if (cents % 25 >= 0)
+		{
+			coins += cents / 25;
+			cents = cents % 25;
+		}
+		if (cents % 10 >= 0)
+		{
+			coins += cents / 10;
+			cents = cents % 10;
+		}
+		if (cents % 5 >= 0)
+		{
+			cents += cents / 5;
+			cents = cents % 5;
+		}
+		if (cents % 2 >= 0)
+		{
+			coins += cents / 2;
+			cents = cents % 2;
+		}
+		if (cents % 1 >= 0)
+			coins += cents;
+		printf("%d\n", coins);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		if (argc[1] < 0)
-			printf("%d\n", 0);
-		else
-			
-	}
-
-	return
 }
-
