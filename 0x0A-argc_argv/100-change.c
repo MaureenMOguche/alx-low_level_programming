6a21b9d4863e89c0df49d1955c5cd1b254ae7063
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-22 16:02:26 -0700

    fin

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,50 +4,49 @@
 
 /**
  * main - prints minimum number of coins to make change for money
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: 0 (success) or 1 (error)
  */
 
 int main(int argc, char **argv)
 {
-	int coins = 0;
-	int cents;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		cents = atoi(argv[1]);
-		if (cents < 0)
+		printf("Error\n");
+		return (1);
+	}
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
 		{
-			printf("%d\n", 0);
-			return (0);
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
 		}
-		if (cents % 25 >= 0)
-		{
-			coins += cents / 25;
-			cents = cents % 25;
-		}
-		if (cents % 10 >= 0)
-		{
-			coins += cents / 10;
-			cents = cents % 10;
-		}
-		if (cents % 5 >= 0)
-		{
-			cents += cents / 5;
-			cents = cents % 5;
-		}
-		if (cents % 2 >= 0)
-		{
-			coins += cents / 2;
-			cents = cents % 2;
-		}
-		if (cents % 1 >= 0)
-			coins += cents;
-		printf("%d\n", coins);
-		return (0);
+		if (total == 1)
+			count++;
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	printf("%d\n", count);
+	return (0);
 }
