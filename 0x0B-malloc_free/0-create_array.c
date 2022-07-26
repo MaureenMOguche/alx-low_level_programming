93f016f0d972bd77f487ebaa137a4e11918d1c55
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-26 09:58:06 -0700

    zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
+		if (ptr_c == NULL)
+        	{
+			return (NULL);
+        	}
+
 		*(ptr_c + i) = c;
 	}
 	return (ptr_c);
