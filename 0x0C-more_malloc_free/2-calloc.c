94fe4979e7d7bd97e9d63975ccf1a81063b5f11e
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-28 13:35:24 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,10 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * sizeof(size));
 
 	if (ptr == NULL)
 		return (NULL);
