3d8b6b7dc99bb0f668352774be247ccdc27de9f4
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-07-28 13:09:34 -0700

    onee

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = s2_len;
 	}
 
-	ptr = malloc((s1_len + n + 1) * sizeof(int));
+	ptr = malloc((s1_len + n + 1) * sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
@@ -53,9 +53,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 
 	for (i = 0; i < n; i++)
-	{
 		ptr[s1_len + i] = s2[i];
-	}
 
 	ptr[s1_len + n + 1] = '\0';
 
