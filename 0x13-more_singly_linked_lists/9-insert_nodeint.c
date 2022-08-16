7a7a6f1984a11d0a08006c5f8c81ec63f73779e1
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-08-16 02:54:04 +0100

    nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,22 +12,39 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newnode, *temp;
-	unsigned int a = 0;
+	unsigned int a = 0, count = 0;
 
+	/* assign memory for new node element */
 	newnode = malloc(sizeof(listint_t));
 
 	if (!newnode)
 	{
 		return (NULL);
 	}
-
 	newnode->n = n;
+
+	/* count number of elements in list */
 	temp = *head;
-	while (a < idx)
+	while (temp)
 	{
+		count++;
 		temp = temp->next;
-		a++;
 	}
-	temp = newnode;
-	return (temp);
+
+	/* check if index is greater than no. of elements */
+	if (a > count)
+	{
+		return (NULL);
+	}
+	else
+	{
+		temp = *head;
+		while (a < idx) /* iterate to idx and insert new node */
+		{
+			temp = temp->next;
+			a++;
+		}
+		temp = newnode;
+		return (temp);
+	}
 }
