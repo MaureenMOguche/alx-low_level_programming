97a73b70430177821e361c31188d69b5f372a4c5
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-08-18 22:08:45 +0100

    betty one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * binary_to_unit - converts a binary to an unsigned int
+ * binary_to_uint - converts a binary to an unsigned int
  * @b: binary to be converted
  * Return: the converted number or 0;
  */
@@ -11,7 +11,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, sum = 0;
-	
+
 	if (b == NULL)
 	{
 		return (0);
