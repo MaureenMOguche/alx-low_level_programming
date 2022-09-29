98d05f6efec2f113f971136dbe5ab24bfc60a222
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-30 00:53:39 +0100

    seve

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->n = n;
 
 	temp = *h;
-	while (temp != 0)
+	while (temp != NULL)
 	{
 		temp = temp->next;
 		count++;
