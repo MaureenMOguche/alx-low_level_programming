8a67217f3da98d13550d92013617d070f116e91a
MaureenMOguche <career.oguchemaureen@outlook.com>
2022-09-30 08:47:03 +0100

    two

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -30,8 +30,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		newnode->next = *head;
 		newnode->prev = NULL;
 		*head = newnode;
-		return (newnode);
 	}
 
-	return (NULL);
+	return (newnode);
 }
